a517b16c15b75c00b0409f13be27f8e2491406bc
Akkawat Tanan <kop_akkawat22@hotmail.com>
2018-04-05 16:52:19 +0700

    create project pow.

---
+++ ProfitOptimizer/Pow/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Pow(5, 4));
+            Console.ReadLine();
+        }
+
+        private static int Pow(int number, int pow)
+        {
+            int countLoop = 0;
+            int loop = number;
+            int result = 0;
+            for (int i = 1; i < pow; i++)
+            {
+                for (int j = 0; j < loop; j++)
+                {
+                    countLoop += number;
+                }
+
+                loop = countLoop;
+                result = countLoop;
+
+                countLoop = 0;
+                
+            }
+
+            return result;
+        }
+    }
+}
