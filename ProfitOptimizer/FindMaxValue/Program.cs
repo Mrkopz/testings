12271db5bf3d5acc27c79a02f789e58f1ceed827
Akkawat Tanan <kop_akkawat22@hotmail.com>
2018-04-12 18:51:18 +0700

    create find max value project.

---
+++ ProfitOptimizer/FindMaxValue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrary = new int[] { 1, 5, 4, 3, 2, 6, 10, 8, 9, 7 };
+
+            var result = GetMaxTwo(arrary);
+            var output = string.Format("first value: {0}, second value: {1}", result.Item1, result.Item2);
+
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+
+        private static Tuple<int, int> GetMaxTwo(IEnumerable<int> values)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
